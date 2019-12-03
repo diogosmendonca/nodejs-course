24ca062d3ae25bee6d6fa5b8ff5bdfd3c170db81
Diogo S. Mendonca <diogosmendonca@gmail.com>
2019-12-02 23:17:56 -0300

    Passport OAuth2 Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '448155092540421',
+        clientSecret: '711dc447d01fa3649358651e4665df7f'
+    }
 }
